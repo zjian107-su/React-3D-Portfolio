99e7714abdf77b354e4f5fbe0b2606317dca82f3
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-02-27 12:22:19 -0500

    ✨ update: add logo and nav bar with tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline text-slate-900 border-amber-50 h-3">
-      3D Developer Portfoliooooo
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
