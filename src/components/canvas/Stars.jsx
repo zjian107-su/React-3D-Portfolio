0eab0b9663581950985fd9a4baf505d303bf1efe
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-07-28 10:15:28 -0400

    💎 milestone: contact with emailjs and CI deployment

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -20,7 +20,7 @@ const Stars = (props) => {
         <PointMaterial
           transparent
           color="#f272c8"
-          size={0.002}
+          size={0.006}
           sizeAttenuation={true}
           depthWrite={false}
         />
